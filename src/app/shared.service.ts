dfd1314db3e68abf8f44b3afdb5d631adfa3edb4
DilanBernal <dilanfbernalc@gmail.com>
2024-11-26 19:55:25 -0500

    funcion duplicada shared service

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -8,14 +8,7 @@ export class SharedService {
   public showHeader:boolean = true;
 
   public selectedItem:string = ''
-
-  getSelectedItem():string{
-    return this.selectedItem;
-  }
-
-  setSelectedItem(value:string){
-    this.selectedItem = value;
-  }
+ 
   getShowHeader(): boolean{
     console.log("oasjfoa")
     return this.showHeader;
