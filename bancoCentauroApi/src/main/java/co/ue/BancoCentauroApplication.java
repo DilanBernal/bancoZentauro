57841ab24cf520b480ac169a3c1541a3cfd90255
DilanBernal <dilanfbernalc@gmail.com>
2024-11-28 09:34:58 -0500

    API
    Nixon dijo que subia nota si tenia un api, por lo que para mejorar nota y para practicar la voy a hacer

---
+++ bancoCentauroApi/src/main/java/co/ue/BancoCentauroApplication.java
@@ -0,0 +1,13 @@
+package co.ue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoCentauroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoCentauroApplication.class, args);
+	}
+
+}
---
+++ bancoCentauroApi/src/test/java/co/ue/BancoCentauroApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoCentauroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
