da2d939d922daff4e99eeacb58e763ee737aea2f
Sergioard02 <140711560+Sergioard02@users.noreply.github.com>
2024-12-09 20:02:22 -0500

    internalizacion
    segunda parte de la internalizacion

--- src/app/Content/product-info/estudiantes/estudiantes.component.ts
+++ src/app/Content/product-info/estudiantes/estudiantes.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-estudiantes',
   templateUrl: './estudiantes.component.html',
-  styleUrl: './estudiantes.component.css'
+  styleUrls: ['./estudiantes.component.css']  // Corregido de styleUrl a styleUrls
 })
 export class EstudiantesComponent {
 
-  constructor(private router:Router){}
-
-  solicitudTarjetaEstudiantes(){
-    this.router.navigate(["form-solicitud"]);
+  constructor(private router: Router, private translate: TranslateService) {
+    // Establecer idioma por defecto
+    this.translate.setDefaultLang('es');  // Establece el idioma predeterminado (español)
   }
 
+  // Cambiar el idioma
+  changeLanguage(lang: string) {
+    this.translate.use(lang);  // Cambia el idioma según el parámetro lang
+  }
+
+  // Método para redirigir a la página de solicitud
+  solicitudTarjetaEstudiantes() {
+    this.router.navigate(["form-solicitud"]);  // Navega a la página de solicitud
+  }
 }
