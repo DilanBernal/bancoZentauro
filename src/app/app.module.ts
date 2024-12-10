86b701e5b02a548a6ebb2d42bb6cb9187a7965c4
DilanBernal <dilanfbernalc@gmail.com>
2024-12-10 00:50:47 -0500

    Arreglo api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,11 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AdminModule } from './Admin/admin.module';
-import { ApiService } from './services/api.service';
 import { ContentModule } from './Content/content.module';
 import { PopupModule } from './Content/popup/popup.module';
 import { UsuarioModule } from './Usuario/usuario.module';
-import { ListaProductosCLComponent } from './Usuario/lista-productos-cl/lista-productos-cl.component';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { ListaProductosComponent } from './Admin/productos/lista-productos/lista-productos.component';
 
 @NgModule({
   declarations: [
